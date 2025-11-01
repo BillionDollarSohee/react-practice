1709506cd7496238e46902271340ce3e47ef41da
unknown <blackteatime4@naver.com>
2025-11-01 19:27:13 +0900

    cleanup embedded repo

---
+++ make-component/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Component from './component/Component';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>my first app UI 컴포넌트 사용법</h3>
+        <Component />
+        <Component />
+        <Component />
+        <Component /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ make-component/src/component/Component.js
@@ -0,0 +1,10 @@
+
+function Component(props) {
+    return (
+        <div>
+            <h3>my first Component</h3>
+        </div>
+    ); 
+}
+
+export default Component;
